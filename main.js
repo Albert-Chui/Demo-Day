f52cf0138db78b3aa284ce32d3c1bb37df8d6c06
Albert Chui <calbert0309@gmail.com>
2019-07-10 10:21:20 -0700

    Trying to change letters to Upper Case

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ if (you = 'killed') {
      you = 'are dead'
 }
 else (you = "alive?")
+function Myfunction() {
+var hi =document.querySelector(".square").value
+var res = hi.toUpperCase()
+document.querySelector(".square").innerHTML = res
+}

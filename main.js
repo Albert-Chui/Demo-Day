17221b7fb7bc6875a1d992339ebb172bce8cd4c6
Albert Chui <calbert0309@gmail.com>
2019-07-11 12:56:48 -0700

    Added a hover to the Crossword Puzzle squares

--- main.js
+++ main.js
@@ -9,3 +9,17 @@ var hi = document.querySelector(".square").value
 var res = hi.toUpperCase()
 document.querySelector(".square").innerHTML = res
 }
+function changeText(text)
+{
+    var display = document.getElementById('text-display');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+     
+function defaultText()
+{
+    var display = document.getElementById('text-display');
+    display.innerHTML = "";
+    display.innerHTML = ":Your Hint Here:";
+}
+ 

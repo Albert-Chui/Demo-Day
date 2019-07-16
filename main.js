fe255a3a80259b9ef9fe15b8f78af462dd0d3259
Mission Bit <missionbit@32a-MissionBit.local>
2019-07-16 11:02:33 -0700

    com

--- main.js
+++ main.js
@@ -23,16 +23,38 @@ function defaultText()
     display.innerHTML = ":Your Hint Here:";
 }
  
-
+console.log('running')
 //first make an empty array
 var list = []
 // make array with correct answers
-var list = ["yawn", "dog", "jog", "chop", "pop"];
+var list1 = ["yawn", "dog", "jog", "chop", "pop"];
 //add button for input then make an event handler for that button
-document.querySelector('#y').onclick = 
+document.querySelector('#y').onclick = function(){
+    var input = document.getElementById('text').value;
+        
+    console.log('funcion started')
+    //if else statement 
+    
+
+    
+    
+        for (let i=0; i < list1.length; i++) {
+            if (input == list1[i]) {
+                list[i]=input;
+                console.log('correct'); 
+                break;
+            } else {
+                console.log('incorrect');
+                
+            }
+        
+    }if (list.length == list1.length){
+        var div = ( document.all ) ? document.all['myDiv'] : document.getElementById('myDiv');
+        div.innerHTML = '<a href="memory3.html"><button class="button1">Click to continue</button></a>';
+        console.log('everything is cooreet');
+
+    }
+
+}
 //make a function to add user input to array
-function () {}
-let list = 
-document.querySelector('#text').value
-console.log()
-//if else statement 
+// function () {}

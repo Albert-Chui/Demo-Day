ed9e6eb94effc0c6947cacbd2400094d72490a82
Mission Bit <missionbit@32a-MissionBit.local>
2019-07-19 11:49:29 -0700

    com

--- mem4.js
+++ mem4.js
@@ -1,50 +1,45 @@
-console.log('running')
-//first make an empty array
-var list = []
-// make array with correct answers
-var list1 = ["shoot", "blue", "orange", "green", "sock", "rock", "cat", "fox", "paper", "desert"];
-// var hide = document.getElementById("new");
-// hide.style.display = "none";
-document.getElementById("new").style.display = "none";
-//add button for input then make an event handler for that button
-document.querySelector('#n').onclick = function(){
-    var input = document.getElementById('text').value;
-     
-    console.log('funcion started')
-    //if else statement 
-    
+window.onload = function() {
+    console.log('running')
 
+    var list = []
     
+    var list1 = ["hop", "sob", "knot", "drop", "sock"];
+  
+    document.getElementById("new").style.display = "none";
     
-        for (let i=0; i < list1.length; i++) {
-            if (input == list1[i]) {
-                list[i]=input;
-                console.log('correct'); 
-                document.getElementById('result').innerHTML = 'correct';
-                break;
-            } else {
-                console.log('incorrect');
-                document.getElementById('result').innerHTML = 'incorrect';
-            }
+    document.querySelector('#m').onclick = function(){
+        var input = document.getElementById('text').value;
+         
+        console.log('funcion started')
+  
         
-     }
-     let b = 0;
-     for (let j=0; j < list1.length; j++) {
-         if (list[j] == list1[j]){
-             b++; 
-             console.log(b);
-             if (b == list1.length) {
-                 document.getElementById("new").style.display = "block";
-                 break;
+    
+        
+        
+            for (let i=0; i < list1.length; i++) {
+                if (input == list1[i]) {
+                    list[i]=input;
+                    console.log('correct'); 
+                    document.getElementById('result').innerHTML = 'correct';
+                    break;
+                } else {
+                    console.log('incorrect');
+                    document.getElementById('result').innerHTML = 'incorrect';
+                }
+            
+         }
+         let b = 0;
+         for (let j=0; j < list1.length; j++) {
+             if (list[j] == list1[j]){
+                 b++; 
+                 console.log(b);
+                 if (b == list1.length) {
+                     document.getElementById("new").style.display = "block";
+                     break;
+                 }
              }
          }
-     }
-     //if (list.length == list1.length){
-        
-    //     console.log('everything is cooreet');
-    //     document.getElementById("new").style.display = "block";
-
-    // }
-
+    
+    }
 }
-
+    
